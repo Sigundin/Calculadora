51775128d8dfc8fa312a5fdd4670194acafe4aac
Afrânio Segundo <113372496+Sigundin@users.noreply.github.com>
2023-03-09 16:48:54 -0300

    Evento clique e display
    Adicionado evento clique no teclado de números e números aparecem no display.

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 var teclaNumero = document.querySelectorAll('.botaoNumero');
 var valorTela = document.querySelector('#telaValor');
 
+//iteração do teclado
 for(i=0; i<teclaNumero.length; i++){
    teclaNumero[i].addEventListener('click', (evento) =>{
-    console.log(evento)
-    })
+    //mostra o valor clicado na tela
+    valorTela.value += evento.target.value
     
+    })
+
 }
+
